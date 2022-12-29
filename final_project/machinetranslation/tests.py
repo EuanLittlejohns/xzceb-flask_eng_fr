02fc15bb65bb009067cb93f73c151c57bed2f8bf
EuanLittlejohns <euanlittlejohns@gmail.com>
2022-12-29 14:24:02 -0500

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -5,10 +5,12 @@ class Test_french_to_english(unittest.TestCase):
     def test1(self): 
         self.assertIsNone(french_to_english(None))
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Hello'), 'Bonjour')
 
 class Test_english_to_french(unittest.TestCase):
     def test1(self):
         self.assertIsNone(english_to_french(None))
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('Bonjour'), 'Hello')
 
 unittest.main()
